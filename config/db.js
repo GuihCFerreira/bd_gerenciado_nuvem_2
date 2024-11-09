c6abd0b89ce2dc9c1607c31da5a3935a5a87a5a3
Lucas <lucasgeansantos@gmail.com>
2024-11-09 11:09:57 -0300

    refactor: refatoração do código

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
 const Sequelize = require('sequelize');
 
 const sequelizeConfig = {
-    dialect: 'postgres', //mudar para mysql 
-    port: 5432,
-    host: '0.0.0.0', //Colocar o IP do banco principal (write)
+    dialect: 'mysql', //mudar para mysql 
+    port: 3306,
+    host: '35.193.202.55', //Colocar o IP do banco principal (write)
     logging: false,
     replication: {
         read: [
           {
-            host: '0.0.0.0',  //Colocar o IP do banco secundário (read)
-            username: 'postgres', //Colocar o usuário do banco secundário (read)
-            password: 'postgres', //Colocar a senha do banco secundário (read) 
+            host: '34.41.249.125',  //Colocar o IP do banco secundário (read)
+            username: 'usr_lucas', //Colocar o usuário do banco secundário (read)
+            password: 'b^ZS0RQ3dYF10L-(', //Colocar a senha do banco secundário (read) 
           },
         ],
         write: {
-          host: '0.0.0.0', //Colocar o IP do banco principal (write)
-          username: 'postgres', //Colocar o usuário do banco principal (write)
-          password: 'postgres', //Colocar a senha do banco principal (write)
+          host: '35.193.202.55', //Colocar o IP do banco principal (write)
+          username: 'usr_lucas', //Colocar o usuário do banco principal (write)
+          password: 'b^ZS0RQ3dYF10L-(', //Colocar a senha do banco principal (write)
         },
       },
     pool: {
@@ -25,13 +25,13 @@ const sequelizeConfig = {
         acquire: 30000,
         idle: 10000,
       },
-      dialectOptions: {
-        useUTC: false, 
-      },
+      // dialectOptions: {
+      //   useUTC: false, 
+      // },
       timezone: '-03:00', 
 };
 
 //Colocar na ordem o nome do BD, Usuário e Senha
-const sequelize = new Sequelize('aula', 'postgres', 'postgres', sequelizeConfig);
+const sequelize = new Sequelize('aula', 'usr_lucas', 'b^ZS0RQ3dYF10L-(', sequelizeConfig);
 
 module.exports = sequelize;
